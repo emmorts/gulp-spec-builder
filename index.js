34d40c3bbed1452bc6aca33034dea125bd167a69
Tomas Stropus <tomas.stropus@devbridge.com>
2016-01-19 02:54:52 +0200

    Fixed subdirectory processing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs');
+var process = require('process');
 var through = require('through2');
 var gutil = require('gulp-util');
 
@@ -16,7 +17,7 @@ var spec = function (name) {
     if (fileNameSearch) {
       filename = fileNameSearch[1];
     }
-    var subdirSearch = path.match(/(\w+)\\\w+\.spec\.js$/);
+    var subdirSearch = path.match(/spec\\([a-zA-Z0-9\\]+)\\\w+\.spec\.js$/);
     if (subdirSearch) {
       subdir = subdirSearch[1];
     }
@@ -44,7 +45,7 @@ module.exports = spec;
 var getCurrentSpec = function (dic, subdir) {
   var obj = dic;
   if (subdir) {
-    var fragments = subdir.split('/');
+    var fragments = subdir.split('\\');
     for (var i = 0; i < fragments.length; i++) {
       if (!(fragments[i] in obj)) {
         obj[fragments[i]] = {};
@@ -57,13 +58,13 @@ var getCurrentSpec = function (dic, subdir) {
 
 var buildSpec = function (spec, obj) {
   if (obj.Describe) {
-    spec += '\r\n' + process(obj.Describe) + '\r\n';
+    spec += '\r\n' + indent(obj.Describe) + '\r\n';
   }
   var propertyName,
       subSpec;
   for (propertyName in obj) {
     if (propertyName !== 'Describe' && typeof obj[propertyName] === 'string') {
-      spec += '\r\n' + process(obj[propertyName]) + '\r\n';
+      spec += '\r\n' + indent(obj[propertyName]) + '\r\n';
     }
   }
   for (propertyName in obj) {
@@ -71,38 +72,16 @@ var buildSpec = function (spec, obj) {
       subSpec = '\r\n' + "describe(\"package's '" + propertyName + "' spec\", function () {" + '\r\n';
       subSpec = buildSpec(subSpec, obj[propertyName]);
       subSpec += '\r\n' + "});" + '\r\n';
-      spec += process(subSpec);
+      spec += indent(subSpec);
     }
   }
   return spec;
 };
 
-var process = function (src) {
+var indent = function (src) {
   var lines = src.split('\r\n');
   for (var i = 0; i < lines.length; i++) {
     lines[i] = '  ' + lines[i];
   }
   return lines.join('\r\n');
-};
-
-var buildSpec = function (spec, obj) {
-  if (obj.Describe) {
-    spec += '\r\n' + process(obj.Describe) + '\r\n';
-  }
-  var propertyName,
-      subSpec;
-  for (propertyName in obj) {
-    if (propertyName !== 'Describe' && typeof obj[propertyName] === 'string') {
-      spec += '\r\n' + process(obj[propertyName]) + '\r\n';
-    }
-  }
-  for (propertyName in obj) {
-    if (propertyName !== 'Describe' && typeof obj[propertyName] === 'object') {
-      subSpec = '\r\n' + "describe(\"package's '" + propertyName + "' spec\", function () {" + '\r\n';
-      subSpec = buildSpec(subSpec, obj[propertyName]);
-      subSpec += '\r\n' + "});" + '\r\n';
-      spec += process(subSpec);
-    }
-  }
-  return spec;
 };
